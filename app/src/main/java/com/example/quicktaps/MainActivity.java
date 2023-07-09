f0185362ac505f184ca11cca6fefa276ff774448
sauna1137 <subscription.search@gmail.com>
2023-07-09 18:38:15 +0900

    main activity

--- app/src/main/java/com/example/quicktaps/MainActivity.java
+++ app/src/main/java/com/example/quicktaps/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.quicktaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.title);
+        ((Button)findViewById(R.id.start)).setOnClickListener(this);
+        ((Button)findViewById(R.id.score)).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case(R.id.start):
+                title.setText("HelloWorld");
+            case(R.id.score):
+        }
+     }
 }
