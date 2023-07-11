d9e038c57f117bb42af46bb5513cab469f0926fe
sauna1137 <subscription.search@gmail.com>
2023-07-11 22:13:49 +0900

    highscore追加

--- app/src/main/java/com/example/quicktaps/HighScore.java
+++ app/src/main/java/com/example/quicktaps/HighScore.java
@@ -3,12 +3,24 @@ package com.example.quicktaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HighScore extends AppCompatActivity {
+public class HighScore extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+
+        ((Button)findViewById(R.id.button3)).setOnClickListener(this);
+        ((Button)findViewById(R.id.button4)).setOnClickListener(this);
+        ((Button)findViewById(R.id.button5)).setOnClickListener(this);
+        ((Button)findViewById(R.id.buttonHome)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
