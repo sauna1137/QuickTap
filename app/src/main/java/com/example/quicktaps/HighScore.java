ac0241d95fd26d35440b3c639b30b71f4131bbb5
sauna1137 <subscription.search@gmail.com>
2023-07-13 19:57:24 +0900

    highscore修正

--- app/src/main/java/com/example/quicktaps/HighScore.java
+++ app/src/main/java/com/example/quicktaps/HighScore.java
@@ -21,6 +21,11 @@ public class HighScore extends AppCompatActivity implements View.OnClickListener
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case (R.id.button3): break;
+            case (R.id.button4): break;
+            case (R.id.button5): break;
+            case (R.id.buttonHome): break;
+        }
     }
 }
