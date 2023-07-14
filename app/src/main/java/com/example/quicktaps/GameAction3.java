2457245d8330ae5ee15aa767e2a95d9926841053
sauna1137 <subscription.search@gmail.com>
2023-07-14 22:43:35 +0900

    gameAction3

--- app/src/main/java/com/example/quicktaps/GameAction3.java
+++ app/src/main/java/com/example/quicktaps/GameAction3.java
@@ -3,12 +3,23 @@ package com.example.quicktaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class GameAction3 extends AppCompatActivity {
+public class GameAction3 extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_action);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.button3): break;
+            case (R.id.button4): break;
+            case (R.id.button5): break;
+            case (R.id.buttonHome): break;
+        }
     }
 }
