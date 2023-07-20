b4fcb621294d74c43e86b8b9595b192b9f64ceba
sauna1137 <subscription.search@gmail.com>
2023-07-20 22:35:46 +0900

    gameAction3 追加

--- app/src/main/java/com/example/quicktaps/GameAction3.java
+++ app/src/main/java/com/example/quicktaps/GameAction3.java
@@ -8,12 +8,45 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class GameAction3 extends AppCompatActivity implements View.OnClickListener {
+    private Button b1;
+    private Button b2;
+    private Button b3;
+    private Button b4;
+    private Button b5;
+    private Button b6;
+    private Button b7;
+    private Button b8;
+    private Button b9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_action3);
+        b1 = findViewById(R.id.button1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.button2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.button3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.button4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.button5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.button6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.button7);
+        b7.setOnClickListener(this);
+        b8 = findViewById(R.id.button8);
+        b8.setOnClickListener(this);
+        b9 = findViewById(R.id.button9);
+        b9.setOnClickListener(this);
+
 
         ((Button)findViewById(R.id.button1)).setOnClickListener(this);
         ((Button)findViewById(R.id.button2)).setOnClickListener(this);
@@ -26,6 +59,28 @@ public class GameAction3 extends AppCompatActivity implements View.OnClickListen
         ((Button)findViewById(R.id.button9)).setOnClickListener(this);
         ((Button)findViewById(R.id.buttonHome)).setOnClickListener(this);
         ((Button)findViewById(R.id.buttonRetry)).setOnClickListener(this);
+
+        List<String> number = new ArrayList<String>();
+        number.add("1");
+        number.add("2");
+        number.add("3");
+        number.add("4");
+        number.add("5");
+        number.add("6");
+        number.add("7");
+        number.add("8");
+        number.add("9");
+        Collections.shuffle(number);
+
+        b1.setText(number.get(0));
+        b2.setText(number.get(1));
+        b3.setText(number.get(2));
+        b4.setText(number.get(3));
+        b5.setText(number.get(4));
+        b6.setText(number.get(5));
+        b7.setText(number.get(6));
+        b8.setText(number.get(7));
+        b9.setText(number.get(8));
     }
 
     @Override
